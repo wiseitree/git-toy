714a88bedbd7dacd1fccc343e4dea63d5a689ff6
Soultreee <neverfidgh@naver.com>
2023-09-10 17:40:20 +0900

    add print() To ToyApplication

--- src/main/java/ToyApplication.java
+++ src/main/java/ToyApplication.java
@@ -2,5 +2,6 @@ public class ToyApplication {
 
     public static void main(String[] args) {
         System.out.println("ToyApplication.main");
+        System.out.println("ToyApplication.main2");
     }
 }
