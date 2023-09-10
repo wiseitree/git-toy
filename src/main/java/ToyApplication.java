6e56d8b7e22f40441153da697aee07d3348a177b
Soultreee <neverfidgh@naver.com>
2023-09-10 17:42:20 +0900

    add third_print ToyApplication

--- src/main/java/ToyApplication.java
+++ src/main/java/ToyApplication.java
@@ -3,5 +3,8 @@ public class ToyApplication {
     public static void main(String[] args) {
         System.out.println("ToyApplication.main");
         System.out.println("ToyApplication.main2");
+        System.out.println("ToyApplication.main3");
+
+
     }
 }
