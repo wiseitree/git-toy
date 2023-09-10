3558a83ad27aeab805a7608249d5bda6229fc10d
Soultreee <neverfidgh@naver.com>
2023-09-10 15:50:27 +0900

    Add M_add to Calculator

--- src/main/java/ToyApplication.java
+++ src/main/java/ToyApplication.java
@@ -1,4 +1,5 @@
 public class ToyApplication {
+
     public static void main(String[] args) {
         System.out.println("ToyApplication.main");
     }
---
+++ src/main/java/part1/Calculator.java
@@ -0,0 +1,9 @@
+package part1;
+
+public class Calculator {
+
+    // add
+    public int add(int a, int b){
+        return a + b;
+    }
+}
