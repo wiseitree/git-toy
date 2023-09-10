140b8ea5d150c181464029065c3f38594071f7c0
Soultreee <neverfidgh@naver.com>
2023-09-10 16:02:43 +0900

    커밋되돌리기

--- src/main/java/part1/Calculator.java
+++ src/main/java/part1/Calculator.java
@@ -12,14 +12,5 @@ public class Calculator {
         return a - b;
     }
 
-    // multi
-    public int multi(int a, int b){
-        return a*b;
-    }
-
-    // divide
-    public int divide(int a, int b){
-        return a/b;
-    }
 
 }
