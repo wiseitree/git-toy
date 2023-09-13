ab19baeba1af4d6220a00c1903fdbabca8ac9393
Soultreee <neverfidgh@naver.com>
2023-09-13 15:17:41 +0900

    feat: conflictTest1()
    Add "conflictTest" to FeatureService

--- src/main/java/feature/service/FeatureService.java
+++ src/main/java/feature/service/FeatureService.java
@@ -16,6 +16,16 @@ public class FeatureService {
 
     }
 
+    public void conflictTest1(){
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+        System.out.println("conflictTest1");
+    }
+
     public void test3(){
         System.out.println("test3");
     }
@@ -25,20 +35,6 @@ public class FeatureService {
         System.out.println("test4");
     }
 
-    public void test5(){
-        System.out.println("test5");
-    }
-
-    public void test6(){
-        System.out.println("test6");
-    }
-
-    public void test7(){
-        System.out.println("test7");
-    }
-    public void test8(){
-        System.out.println("test8");
-    }
 
 
 }
